f30a02e2e0b11375135b98e9f534f06ef3ab579d
stelioskarydakis <mrkarydakis@gmail.com>
2023-05-13 09:06:49 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
+
+const App = () => {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <>
+      <div className="app">
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/predictions" element={<Predictions />} />
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
+      </div>
+    </>
+  );
+};
+
+export default App;
